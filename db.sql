835a2b598ab49f8439bd63a57ae4e968d036d85f
= <=>
2025-03-09 22:04:04 +0800

    Update Db & Margin

--- db.sql
+++ db.sql
@@ -5,6 +5,7 @@ CREATE TABLE tugas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nama_tugas VARCHAR(100) NOT NULL,
     deskripsi_tugas TEXT,
+    status ENUM('berlangsung', 'selesai') NOT NULL,
     deadline DATE,
     prioritas ENUM('Tinggi', 'Sedang', 'Rendah') NOT NULL,
     kategori ENUM('Kuliah', 'Kerja', 'Pribadi') NOT NULL
